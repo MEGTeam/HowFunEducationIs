abb2464aa55797ca46956997b5314ec3bd094dcd
Ali-Eldeba <88302640+Ali-Eldeba@users.noreply.github.com>
2022-03-24 18:22:43 +0200

    Looping on images

--- script/main.js
+++ script/main.js
@@ -1,3 +1,20 @@
+/*
+  Looping on All of The images in The Website
+  if there is an img without an "draggable" Attribute it will add "draggable" Attribute
+  else if the image has "draggable" Attribute and it is "false" it will be "false"
+  else the image has "draggable" Attribute and it is "true" it will be "true"
+*/
+// Strting the Loop
+for (i = 0; i < document.images.length; i++) {
+  if (document.images[i].draggable === "") {
+    // images that have "draggable" Attribute and its Value is Empty
+    document.images[i].setAttribute("draggable", "false");
+  } else {
+    // Images Does not Have "draggable" Attribute
+    document.images[i].setAttribute("draggable", "false");
+  }
+}
+
 const showHide = (e) => {
     let myNav = document.querySelector(".sidenav");
     let myBlackEl = document.querySelector(".black_");
